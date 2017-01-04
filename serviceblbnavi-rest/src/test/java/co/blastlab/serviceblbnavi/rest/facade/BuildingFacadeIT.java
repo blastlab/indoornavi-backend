bd254295c01bc93c0c508982e8f32252bdb07a0e
Mateusz Cichosz <mcichosz@blastlab.co>
2017-01-04 13:52:27 +0100

    added database configuration for tests

--- serviceblbnavi-rest/src/test/java/co/blastlab/serviceblbnavi/rest/facade/BuildingFacadeIT.java
+++ serviceblbnavi-rest/src/test/java/co/blastlab/serviceblbnavi/rest/facade/BuildingFacadeIT.java
@@ -11,6 +11,7 @@ public class BuildingFacadeIT extends BaseIT {
 
 	private static final String BUILDING_PATH = "/building";
 	private static final String TEST_NAME = "AAAAAdfA";
+	private static final String EXISTING_NAME = "AABBCCDD";
 
 	@Test
 	public void createNewBuilding() {
@@ -27,4 +28,20 @@ public class BuildingFacadeIT extends BaseIT {
 				"name", equalTo(TEST_NAME)
 			);
 	}
+
+/*	@Test
+	public void updateBuilding() {
+		String body = new RequestBodyBuilder("BuildingCreating.json")
+			.setParameter("name", EXISTING_NAME)
+			.setParameter("complexId",2)
+			.build();
+
+		givenUser()
+			.body(body)
+			.when().put(BUILDING_PATH)
+			.then().statusCode(HttpStatus.SC_OK)
+			.body(
+				"name", equalTo(TEST_NAME)
+			);
+	}*/
 }
