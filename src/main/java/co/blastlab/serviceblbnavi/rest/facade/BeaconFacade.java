852668f0065f7463b307c0ceb5c178ecb79cefe8
Joanna Smółkowska <jsmolkowska@blaslab.co>
2017-02-10 16:38:36 +0100

    Facade was seperated into two files: interface (new facade file) and logic (new EJB file).

--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/BeaconFacade.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/BeaconFacade.java
@@ -1,128 +1,53 @@
 package co.blastlab.serviceblbnavi.rest.facade;
 
-import co.blastlab.serviceblbnavi.dao.PermissionBean;
-import co.blastlab.serviceblbnavi.dao.repository.BeaconRepository;
-import co.blastlab.serviceblbnavi.dao.repository.FloorRepository;
+
 import co.blastlab.serviceblbnavi.domain.Beacon;
-import co.blastlab.serviceblbnavi.domain.Floor;
-import co.blastlab.serviceblbnavi.domain.Permission;
-import co.blastlab.serviceblbnavi.rest.bean.AuthorizationBean;
 import com.wordnik.swagger.annotations.*;
 
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-/**
- *
- * @author Michał Koszałka
- */
+
 @Path("/beacon")
 @Api("/beacon")
-@Stateless
-public class BeaconFacade {
-
-    @Inject
-    private BeaconRepository beaconRepository;
-
-    @Inject
-    private FloorRepository floorRepository;
-
-    @Inject
-    private PermissionBean permissionBean;
-
-    @Inject
-    private AuthorizationBean authorizationBean;
+public interface BeaconFacade {
 
     @POST
     @ApiOperation(value = "create beacon", response = Beacon.class)
     @ApiResponses({
-        @ApiResponse(code = 404, message = "floor id empty or floor doesn't exist")
+            @ApiResponse(code = 404, message = "floor id empty or floor doesn't exist")
     })
-    public Beacon create(@ApiParam(value = "beacon", required = true)  Beacon beacon) {
-        if (beacon.getFloorId() != null) {
-            Floor floor = floorRepository.findBy(beacon.getFloorId());
-            if (floor != null) {
-                permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                        floor.getBuilding().getComplex().getId(), Permission.UPDATE);
-                beacon.setFloor(floor);
-                beaconRepository.save(beacon);
-                return beacon;
-            }
-        }
-        throw new EntityNotFoundException();
-    }
+    Beacon create(@ApiParam(value = "beacon", required = true) Beacon beacon);
 
-    @GET
-    @Path("/{id: \\d+}")
-    @ApiOperation(value = "find beacon", response = Beacon.class)
+    @PUT
+    @ApiOperation(value = "update beacon", response = Beacon.class)
     @ApiResponses({
-        @ApiResponse(code = 404, message = "beacon with given id wasn't found")
+            @ApiResponse(code = 404, message = "floor id or floor empty or doesn't exist")
     })
-    public Beacon find(@PathParam("id") @ApiParam(value = "id", required = true) Long id) {
-        Beacon beacon = beaconRepository.findBy(id);
-        if (beacon != null) {
-            permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                    beacon.getFloor().getBuilding().getComplex().getId(), Permission.READ);
-            return beacon;
-        }
-        throw new EntityNotFoundException();
-    }
+    Beacon update(@ApiParam(value = "beacon", required = true) Beacon beacon);
 
     @DELETE
     @Path("/{id: \\d+}")
     @ApiOperation(value = "delete beacon", response = Response.class)
     @ApiResponses({
-        @ApiResponse(code = 404, message = "beacon with given id doesn't exist")
+            @ApiResponse(code = 404, message = "beacon with given id doesn't exist")
     })
-    public Response delete(@PathParam("id") @ApiParam(value = "id", required = true) Long id) {
-        Beacon beacon = beaconRepository.findBy(id);
-        if (beacon != null) {
-            permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                    beacon.getFloor().getBuilding().getComplex().getId(), Permission.UPDATE);
-            beaconRepository.remove(beacon);
-            return Response.ok().build();
-        }
-        throw new EntityNotFoundException();
-    }
+    Response delete(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
 
-    @PUT
-    @ApiOperation(value = "update beacon", response = Beacon.class)
+    @GET
+    @Path("/{id: \\d+}")
+    @ApiOperation(value = "find beacon", response = Beacon.class)
     @ApiResponses({
-        @ApiResponse(code = 404, message = "floor id or floor empty or doesn't exist")
+            @ApiResponse(code = 404, message = "beacon with given id wasn't found")
     })
-    public Beacon update(@ApiParam(value = "beacon", required = true) Beacon beacon) {
-        if (beacon.getFloorId() != null) {
-            Floor floor = floorRepository.findBy(beacon.getFloorId());
-            if (floor != null) {
-                permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                        floor.getBuilding().getComplex().getId(), Permission.UPDATE);
-                beacon.setFloor(floor);
-                beaconRepository.save(beacon);
-                return beacon;
-            }
-        }
-        throw new EntityNotFoundException();
-    }
+    Beacon find(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
 
     @GET
     @Path("/floor/{id: \\d+}")
     @ApiOperation(value = "find beacons by floor id", response = Beacon.class)
     @ApiResponses({
-        @ApiResponse(code = 404, message = "floorId empty or floor doesn't exist")
+            @ApiResponse(code = 404, message = "floorId empty or floor doesn't exist")
     })
-    public List<Beacon> findAll(@PathParam("id") @ApiParam(value = "id", required = true) Long floorId) {
-        if (floorId != null) {
-            Floor floor = floorRepository.findBy(floorId);
-            if (floor != null) {
-                permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                        floor.getBuilding().getComplex().getId(), Permission.READ);
-                return beaconRepository.findByFloor(floor);
-            }
-        }
-        throw new EntityNotFoundException();
-    }
+    List<Beacon> findAll(@PathParam("id") @ApiParam(value = "id", required = true) Long floorId);
 }
--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/BuildingFacade.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/BuildingFacade.java
@@ -1,214 +1,97 @@
 package co.blastlab.serviceblbnavi.rest.facade;
 
-import co.blastlab.serviceblbnavi.dao.BuildingConfigurationBean;
-import co.blastlab.serviceblbnavi.dao.PermissionBean;
-import co.blastlab.serviceblbnavi.dao.repository.BuildingRepository;
-import co.blastlab.serviceblbnavi.dao.repository.ComplexRepository;
+
 import co.blastlab.serviceblbnavi.domain.Building;
-import co.blastlab.serviceblbnavi.domain.BuildingConfiguration;
-import co.blastlab.serviceblbnavi.domain.Complex;
-import co.blastlab.serviceblbnavi.domain.Permission;
-import co.blastlab.serviceblbnavi.rest.bean.AuthorizationBean;
 import co.blastlab.serviceblbnavi.views.View;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.wordnik.swagger.annotations.*;
 
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-/**
- *
- * @author Michał Koszałka
- */
 @Path("/building")
 @Api("/building")
-@Stateless
-public class BuildingFacade {
-
-    @Inject
-    private BuildingRepository buildingRepository;
-
-    @Inject
-    private ComplexRepository complexRepository;
-
-    @Inject
-    private PermissionBean permissionBean;
-
-    @Inject
-    private BuildingConfigurationBean buildingConfigurationBean;
-
-    @Inject
-    private AuthorizationBean authorizationBean;
+public interface BuildingFacade {
 
     @POST
     @ApiOperation(value = "create", response = Building.class)
-    public Building create(@ApiParam(value = "building", required = true) Building building) {
-        if (building.getComplexId() != null) {
+    Building create(@ApiParam(value = "building", required = true) Building building);
 
-            permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                    building.getComplexId(), Permission.UPDATE);
-            Complex complex = complexRepository.findBy(building.getComplexId());
-            if (complex != null) {
-                building.setComplex(complex);
-                buildingRepository.save(building);
-                return building;
-            }
-        }
-        throw new EntityNotFoundException();
-    }
+    @PUT
+    @ApiOperation(value = "update building", response = Building.class)
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "complex id or complex empty or doesn't exist")
+    })
+    Building update(@ApiParam(value = "building", required = true) Building building);
+
+    @DELETE
+    @Path("/{id: \\d+}")
+    @ApiOperation(value = "delete building", response = Response.class)
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "building with given id doesn't exist")
+    })
+    Response delete(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
 
     @GET
     @Path("/{id: \\d+}")
     @JsonView(View.BuildingInternal.class)
     @ApiOperation(value = "find building")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "building with given id wasn't found")
+            @ApiResponse(code = 404, message = "building with given id wasn't found")
     })
-    public Building find(@PathParam("id") @ApiParam(value = "id", required = true) Long id) {
-        Building building = buildingRepository.findBy(id);
-
-        if (building != null) {
-            permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                    building.getComplex().getId(), Permission.READ);
-            return building;
-        }
-        throw new EntityNotFoundException();
-    }
-
-    @PUT
-    @ApiOperation(value = "update building", response = Building.class)
-    @ApiResponses({
-        @ApiResponse(code = 404, message = "complex id or complex empty or doesn't exist")
-    })
-    public Building update(@ApiParam(value = "building", required = true) Building building) {
-        if (building.getId() != null) {
-            Complex complex = complexRepository.findByBuildingId(building.getId());
-            if (complex != null) {
-                permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                        complex.getId(), Permission.UPDATE);
-                building.setComplex(complex);
-                buildingRepository.save(building);
-                return building;
-            }
-        }
-        throw new EntityNotFoundException();
-    }
-
-    @DELETE
-    @Path("/{id: \\d+}")
-    @ApiOperation(value = "delete building", response = Response.class)
-    @ApiResponses({
-        @ApiResponse(code = 404, message = "building with given id doesn't exist")
-    })
-    public Response delete(@PathParam("id") @ApiParam(value = "id", required = true) Long id) {
-        Building building = buildingRepository.findBy(id);
-        if (building != null) {
-            permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                    building.getComplex().getId(), Permission.UPDATE);
-            buildingRepository.remove(building);
-            return Response.ok().build();
-        }
-        throw new EntityNotFoundException();
-    }
+    Building find(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
 
     @GET
     @Path("/complex/{id: \\d+}")
     @JsonView(View.ComplexInternal.class)
     @ApiOperation(value = "find buildings by complex id")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "complex doesn't exist")
+            @ApiResponse(code = 404, message = "complex doesn't exist")
     })
-    public List<Building> findAll(@PathParam("id") @ApiParam(value = "complexId", required = true) Long complexId) {
-        permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                complexId, Permission.READ);
-        if (complexId != null) {
-            Complex complex = complexRepository.findBy(complexId);
-            if (complex != null) {
-                return buildingRepository.findByComplex(complex);
-            }
-        }
-        throw new EntityNotFoundException();
-    }
+    List<Building> findAll(@PathParam("id") @ApiParam(value = "complexId", required = true) Long complexId);
 
     @PUT
     @Path("/{id: \\d+}/config/")
     @ApiOperation(value = "create building's configuration")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "building doesn't exist")
+            @ApiResponse(code = 404, message = "building doesn't exist")
     })
-    public Response saveConfiguration(@PathParam("id") @ApiParam(value = "buildingId", required = true) Long buildingId) {
-        Building building = buildingRepository.findBy(buildingId);
-        if (building != null) {
-            permissionBean.checkPermission(authorizationBean.getCurrentUser().getId(),
-                    building.getComplex().getId(), Permission.UPDATE);
-            if (buildingConfigurationBean.saveConfiguration(building)) {
-                return Response.noContent().build();
-            } else {
-                throw new InternalServerErrorException();
-            }
-        }
-        throw new EntityNotFoundException();
-    }
+    Response saveConfiguration(@PathParam("id") @ApiParam(value = "buildingId", required = true) Long buildingId);
 
     @GET
     @Path("/{id: \\d+}/config/")
     @ApiOperation(value = "finds building's configuration by id")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "building doesn't exist or has no configuration set")
+            @ApiResponse(code = 404, message = "building doesn't exist or has no configuration set")
     })
-    public String getConfiguration(@PathParam("id") @ApiParam(value = "buildingId", required = true) Long buildingId) {
-        BuildingConfiguration buildingConfiguration = buildingConfigurationBean.findByBuildingAndVersion(buildingId, 1);
-        if (buildingConfiguration != null && buildingConfiguration.getConfiguration() != null) {
-            return buildingConfiguration.getConfiguration();
-        }
-        throw new EntityNotFoundException();
-    }
+    String getConfiguration(@PathParam("id") @ApiParam(value = "buildingId", required = true) Long buildingId);
 
     @GET
     @Path("/{complexName}/{buildingName}/config/")
     @ApiOperation(value = "finds building's configuration by complex name and building name")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "building doesn't exist or has no configuration set")
+            @ApiResponse(code = 404, message = "building doesn't exist or has no configuration set")
     })
-    public String getConfigurationByComplexNameAndBuildingName(
+    String getConfigurationByComplexNameAndBuildingName(
             @PathParam("complexName") @ApiParam(value = "complexName", required = true) String complexName,
-            @PathParam("buildingName") @ApiParam(value = "buildingName", required = true) String buildingName) {
-        BuildingConfiguration buildingConfiguration = buildingConfigurationBean.findByComplexNameAndBuildingNameAndVersion(complexName, buildingName, 1);
-        if (buildingConfiguration != null && buildingConfiguration.getConfiguration() != null) {
-            return buildingConfiguration.getConfiguration();
-        }
-        throw new EntityNotFoundException();
-    }
+            @PathParam("buildingName") @ApiParam(value = "buildingName", required = true) String buildingName);
 
     @GET
     @Path("/{complexName}/{buildingName}/configChecksum/")
     @ApiOperation(value = "finds building's configuration's checksum by complex name and building name")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "building doesn't exist or has no configuration checksum set")
+            @ApiResponse(code = 404, message = "building doesn't exist or has no configuration checksum set")
     })
-    public String getConfigurationChecksumByComplexNameAndBuildingName(
+    String getConfigurationChecksumByComplexNameAndBuildingName(
             @PathParam("complexName") @ApiParam(value = "complexName", required = true) String complexName,
-            @PathParam("buildingName") @ApiParam(value = "buildingName", required = true) String buildingName) {
-        BuildingConfiguration buildingConfiguration = buildingConfigurationBean.findByComplexNameAndBuildingNameAndVersion(complexName, buildingName, 1);
-        if (buildingConfiguration != null && buildingConfiguration.getConfigurationChecksum() != null) {
-            return buildingConfiguration.getConfigurationChecksum();
-        }
-        throw new EntityNotFoundException();
-    }
+            @PathParam("buildingName") @ApiParam(value = "buildingName", required = true) String buildingName);
 
     @PUT
     @Path("/{id: \\d+}/restoreConfiguration")
     @ApiOperation(value = "restores saved building's configuration")
     @ApiResponses({
-        @ApiResponse(code = 404, message = "building doesn't exist or has no configuration saved")
+            @ApiResponse(code = 404, message = "building doesn't exist or has no configuration saved")
     })
-    public Building restoreConfiguration(
-            @PathParam("id") @ApiParam(value = "id", required = true) Long id) {
-        return buildingConfigurationBean.restoreConfiguration(id);
-    }
-
+    Building restoreConfiguration(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
 }
