a92ac1da844074301855780ffdeb631efd2bf2ce
Pawel Kuzmicki <pkuzmicki@blastlab.co>
2017-03-01 11:17:05 +0100

    Improve swagger for Floor and Vertex. Fix bug with updating floors' level

--- src/main/java/co/blastlab/serviceblbnavi/dto/person/PersonDto.java
+++ src/main/java/co/blastlab/serviceblbnavi/dto/person/PersonDto.java
@@ -1,5 +1,6 @@
 package co.blastlab.serviceblbnavi.dto.person;
 
+import com.wordnik.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
@@ -9,5 +10,6 @@ import org.hibernate.validator.constraints.Email;
 abstract class PersonDto {
 
 	@Email
+	@ApiModelProperty(example = "user@email.com")
 	private String email;
 }
