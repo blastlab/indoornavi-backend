617541991927ad760d2ec7a4434e7efd28ffe3cc
jsmol <jsmolkowska@blastlab.co>
2017-03-16 15:02:21 +0100

    Added complex and building changes

--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/BuildingFacade.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/BuildingFacade.java
@@ -5,54 +5,44 @@ import co.blastlab.serviceblbnavi.rest.facade.ext.filter.TokenAuthorization;
 import com.wordnik.swagger.annotations.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Path("/buildings")
 @Api("/buildings")
+@TokenAuthorization
 public interface BuildingFacade {
 
 	@POST
-	@TokenAuthorization
-	@ApiOperation(value = "create", response = BuildingDto.New.class)
+	@ApiOperation(value = "create building", response = BuildingDto.WithId.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "complex id doesn't exist or complex empty")
 	})
-	BuildingDto create(@ApiParam(value = "building", required = true) @Valid BuildingDto.New building);
+	BuildingDto.WithId create(@ApiParam(value = "building", required = true) @Valid BuildingDto building);
 
 	@PUT
-	@TokenAuthorization
-	@ApiOperation(value = "update building", response = BuildingDto.class)
+	@Path("/{id: \\d+}")
+	@ApiOperation(value = "update building by id", response = BuildingDto.WithId.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "building doesn't exist or complex doesn't contain given building")
 	})
-	BuildingDto update(@ApiParam(value = "building", required = true) @Valid BuildingDto building);
+	BuildingDto.WithId update(@ApiParam(value = "id", required = true) @PathParam("id") @Valid @NotNull() Long id,
+	                                  @ApiParam(value = "building", required = true) @Valid BuildingDto building);
 
 	@DELETE
 	@Path("/{id: \\d+}")
-	@ApiOperation(value = "delete building", response = Response.class)
+	@ApiOperation(value = "delete building by id", response = Response.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "building with given id doesn't exist")
 	})
-	@TokenAuthorization
 	Response delete(@PathParam("id") @ApiParam(value = "building id", required = true) Long id);
 
 	@GET
 	@Path("/{id: \\d+}")
-	@ApiOperation(value = "find building", response = BuildingDto.class)
+	@ApiOperation(value = "find building by id", response = BuildingDto.WithId.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "building with given id wasn't found")
 	})
-	@TokenAuthorization
-	BuildingDto find(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
-
-	@GET
-	@Path("/complex/{id: \\d+}")
-	@ApiOperation(value = "find buildings by complex id", response = BuildingDto.class, responseContainer = "List")
-	@ApiResponses({
-		@ApiResponse(code = 404, message = "complex doesn't exist")
-	})
-	@TokenAuthorization
-	List<BuildingDto> findAll(@PathParam("id") @ApiParam(value = "complexId", required = true) Long complexId);
+	BuildingDto.WithId find(@PathParam("id") @ApiParam(value = "id", required = true) Long id);
 }
--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/ComplexFacade.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/ComplexFacade.java
@@ -16,17 +16,17 @@ import java.util.List;
 public interface ComplexFacade {
 
 	@POST
-	@ApiOperation(value = "create complex", response = ComplexDto.class)
-	ComplexDto create(@ApiParam(value = "complex", required = true) @Valid ComplexDto complex);
+	@ApiOperation(value = "create complex", response = ComplexDto.WithId.class)
+	ComplexDto.WithId create(@ApiParam(value = "complex", required = true) @Valid ComplexDto complex);
 
 	@PUT
 	@Path("/{id: \\d+}")
-	@ApiOperation(value = "update complex by id", response = ComplexDto.class)
+	@ApiOperation(value = "update complex by id", notes = "update complex by idss", response = ComplexDto.WithId.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "complex id empty or complex doesn't exist")
 	})
-	ComplexDto update(@ApiParam(value = "id", required = true) @PathParam("id") @Valid @NotNull Long id,
-	                  @ApiParam(value = "complex", required = true) @Valid ComplexDto complex);
+	ComplexDto.WithId update(@ApiParam(value = "id", required = true) @PathParam("id") @Valid @NotNull Long id,
+	                         @ApiParam(value = "complex", required = true) @Valid ComplexDto complex);
 
 	@DELETE
 	@Path("/{id: \\d+}")
@@ -37,17 +37,14 @@ public interface ComplexFacade {
 	Response delete(@ApiParam(value = "id", required = true) @PathParam("id") @Valid @NotNull Long id);
 
 	@GET
-	@ApiOperation(value = "find all complexes", response = ComplexDto.class, responseContainer = "List")
-	@ApiResponses({
-		@ApiResponse(code = 404, message = "complexes don't exist")
-	})
-	List<ComplexDto> findAll();
+	@ApiOperation(value = "find all complexes", response = ComplexDto.WithId.class, responseContainer = "List")
+	List<ComplexDto.WithId> findAll();
 
 	@GET
 	@Path("/{id: \\d+}/buildings")
-	@ApiOperation(value = "get complex by id (include buildings)", response = ComplexDto.WithBuildings.class)
+	@ApiOperation(value = "get complex by id (include buildings)", response = ComplexDto.WithId.WithBuildings.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "complex id empty or complex doesn't exist")
 	})
-	ComplexDto.WithBuildings findWithBuildings(@ApiParam(value = "id", required = true) @PathParam("id") @Valid @NotNull Long id);
+	ComplexDto.WithId.WithBuildings findWithBuildings(@ApiParam(value = "id", required = true) @PathParam("id") @Valid @NotNull Long id);
 }
