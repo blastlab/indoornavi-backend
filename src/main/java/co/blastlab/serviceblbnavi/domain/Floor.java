6f3d173571ef2cc7bc61b1907d6987a4f90de19e
jsmolkowska <jsmolkowska@blastlab.co>
2017-03-22 15:01:21 +0100

    Added floor changes - working process

--- src/main/java/co/blastlab/serviceblbnavi/domain/Floor.java
+++ src/main/java/co/blastlab/serviceblbnavi/domain/Floor.java
@@ -5,13 +5,20 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Getter
 @Setter
+@Table(
+	uniqueConstraints = @UniqueConstraint(columnNames = {"level", "building_id"})
+)
 public class Floor extends TrackedEntity {
 
 	private Integer level;
+
+	private String name;
 
 	@ManyToOne
 	private Building building;
