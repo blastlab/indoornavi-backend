4c90000940ac9592e32f719beadf9f62742f433b
Karol Gebert <kgebert@blastlab.co>
2017-03-29 11:55:16 +0200

    issue-102 Work in progress

--- src/main/java/co/blastlab/serviceblbnavi/socket/AnchorRegistration.java
+++ src/main/java/co/blastlab/serviceblbnavi/socket/AnchorRegistration.java
@@ -1,10 +1,17 @@
 package co.blastlab.serviceblbnavi.socket;
 
+import co.blastlab.serviceblbnavi.dao.repository.AnchorRepository;
+import co.blastlab.serviceblbnavi.domain.Anchor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @ApplicationScoped
@@ -13,6 +20,9 @@ public class AnchorRegistration extends WebSocketServerAbstract {
 
 	private static Set<Session> clientSessions = Collections.synchronizedSet(new HashSet<Session>());
 	private static Set<Session> serverSessions = Collections.synchronizedSet(new HashSet<Session>());
+
+	@Inject
+	private AnchorRepository anchorRepository;
 
 	@OnOpen
 	public void registerSession(Session session) {
@@ -30,9 +40,11 @@ public class AnchorRegistration extends WebSocketServerAbstract {
 	}
 
 	@OnMessage
-	public void handleMessage(String message, Session session) {
+	public void handleMessage(String message, Session session) throws JsonProcessingException {
 		if (isClientSession(session)) {
-			// nothing here right now
+			List<Anchor> anchorList = anchorRepository.findByVerified(true);
+			ObjectMapper objectMapper = new ObjectMapper();
+			broadCastMessage(clientSessions, objectMapper.writeValueAsString(anchorList));
 		} else if (isServerSession(session)) {
 			// register new anchor and broadcast message to clients
 		}
