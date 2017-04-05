83ea502c131049eef63f7f8507e02dcd3589826a
Karol Gebert <kgebert@blastlab.co>
2017-04-05 12:15:20 +0200

    issue-102 Changes to websocket for anchor registration so it can now broadcast to tags and anchors. Auto broadcast to anchors when created or updated.

--- src/main/java/co/blastlab/serviceblbnavi/socket/AnchorRegistration.java
+++ src/main/java/co/blastlab/serviceblbnavi/socket/AnchorRegistration.java
@@ -3,6 +3,7 @@ package co.blastlab.serviceblbnavi.socket;
 import co.blastlab.serviceblbnavi.dao.repository.AnchorRepository;
 import co.blastlab.serviceblbnavi.domain.Anchor;
 import co.blastlab.serviceblbnavi.dto.anchor.AnchorDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,22 +15,48 @@ import java.util.*;
 
 @ApplicationScoped
 @ServerEndpoint("/anchors/registration")
-public class AnchorRegistration extends WebSocketServerAbstract {
+public class AnchorRegistration {
 
-	private static Set<Session> clientSessions = Collections.synchronizedSet(new HashSet<Session>());
-	private static Set<Session> serverSessions = Collections.synchronizedSet(new HashSet<Session>());
+	private static Set<Session> anchorSessions = Collections.synchronizedSet(new HashSet<Session>());
+	private static Set<Session> tagSessions = Collections.synchronizedSet(new HashSet<Session>());
+	private static Set<Session> sinkSessions = Collections.synchronizedSet(new HashSet<Session>());
 
 	@Inject
 	private AnchorRepository anchorRepository;
 
+	public static void broadcastNewAnchor(Anchor anchor) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		broadCastMessage(anchorSessions, objectMapper.writeValueAsString(Collections.singletonList(new AnchorDto(anchor))));
+	}
+
 	@OnOpen
-	public void registerSession(Session session) {
-		super.registerSession(session);
+	public void registerSession(Session session) throws JsonProcessingException {
+		String queryString = session.getQueryString();
+		ObjectMapper objectMapper = new ObjectMapper();
+		if (SessionType.SINK.getName().equals(queryString)) {
+			sinkSessions.add(session);
+		} else if (SessionType.ANCHOR.getName().equals(queryString)) {
+			anchorSessions.add(session);
+			List<AnchorDto> anchors = new ArrayList<>();
+			anchorRepository.findAll().forEach((anchor) -> {
+				anchors.add(new AnchorDto(anchor));
+			});
+			broadCastMessage(anchorSessions, objectMapper.writeValueAsString(anchors));
+		} else if (SessionType.TAG.getName().equals(queryString)) {
+			tagSessions.add(session);
+		}
 	}
 
 	@OnClose
 	public void unregisterSession(Session session) {
-		super.unregisterSession(session);
+		String queryString = session.getQueryString();
+		if (SessionType.SINK.getName().equals(queryString)) {
+			sinkSessions.remove(session);
+		} else if (SessionType.ANCHOR.getName().equals(queryString)) {
+			anchorSessions.remove(session);
+		} else if (SessionType.TAG.getName().equals(queryString)) {
+			tagSessions.remove(session);
+		}
 	}
 
 	@OnError
@@ -40,11 +67,7 @@ public class AnchorRegistration extends WebSocketServerAbstract {
 	@OnMessage
 	public void handleMessage(String message, Session session) throws IOException {
 		ObjectMapper objectMapper = new ObjectMapper();
-		if (isClientSession(session)) {
-			List<AnchorDto> anchors = new ArrayList<>();
-			anchorRepository.findAll().forEach(anchor -> anchors.add(new AnchorDto(anchor)));
-			broadCastMessage(clientSessions, objectMapper.writeValueAsString(anchors));
-		} else if (isServerSession(session)) {
+		if (Objects.equals(session.getQueryString(), SessionType.SINK.getName())) {
 			AnchorDto anchorDto = objectMapper.readValue(message, AnchorDto.class);
 			Anchor anchorEntity = new Anchor();
 			anchorEntity.setShortId(anchorDto.getShortId());
@@ -54,17 +77,19 @@ public class AnchorRegistration extends WebSocketServerAbstract {
 			anchorEntity.setY(anchorDto.getY());
 			anchorEntity.setVerified(false);
 			anchorEntity = anchorRepository.save(anchorEntity);
-			broadCastMessage(clientSessions, objectMapper.writeValueAsString(Collections.singletonList(new AnchorDto(anchorEntity))));
+			broadCastMessage(anchorSessions, objectMapper.writeValueAsString(Collections.singletonList(new AnchorDto(anchorEntity))));
 		}
 	}
 
-	@Override
-	Set<Session> getClientSessions() {
-		return clientSessions;
-	}
-
-	@Override
-	Set<Session> getServerSessions() {
-		return serverSessions;
+	private static void broadCastMessage(final Set<Session> sessions, final String message) {
+		sessions.forEach(session -> {
+			try {
+				if (session.isOpen()) {
+					session.getBasicRemote().sendText(message);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
