206b0107f49f8045a2d0b0aeadb124a1a3be4598
jsmolkowska <jsmolkowska@blastlab.co>
2017-04-11 12:40:55 +0200

    Partial upload image

--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/image/ImageFacade.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/image/ImageFacade.java
@@ -2,13 +2,11 @@ package co.blastlab.serviceblbnavi.rest.facade.image;
 
 import co.blastlab.serviceblbnavi.dto.floor.ImageUpload;
 import co.blastlab.serviceblbnavi.ext.filter.TokenAuthorization;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,18 +14,19 @@ import java.io.IOException;
 
 @Path("/images")
 @Api("/images")
-@Consumes(MediaType.MULTIPART_FORM_DATA)
+
 @TokenAuthorization
 public interface ImageFacade {
 
 	@POST
+	@Path("/{floorId: \\d+}")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@ApiOperation(value = "upload image of the floor", response = Response.class)
 	@ApiResponses({
 		@ApiResponse(code = 404, message = "Floor with given id does not exist")
 	})
-	Response uploadImage(@MultipartForm @Valid ImageUpload imageUpload//, @MultipartForm
-	                    // @ApiParam  ( value = "attachmentparam", type=MediaType.APPLICATION_OCTET_STREAM) Attachment attachment
-	) throws IOException;
+	Response uploadImage(@ApiParam(value = "floorId", required = true) @PathParam("floorId") @Valid @NotNull Long floorId,
+		@Valid @MultipartForm ImageUpload image) throws IOException;
 
 	@GET
 	@Path("/{id: \\d+}")
