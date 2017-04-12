ad8e6426cee63c286884039d73d0e9600e0fc924
jsmolkowska <jsmolkowska@blastlab.co>
2017-04-12 16:03:27 +0200

    Added GET /images/configuration

--- src/main/java/co/blastlab/serviceblbnavi/ext/mapper/accessory/FileMessagePack.java
+++ src/main/java/co/blastlab/serviceblbnavi/ext/mapper/accessory/FileMessagePack.java
@@ -1,10 +1,8 @@
 package co.blastlab.serviceblbnavi.ext.mapper.accessory;
 
-import static co.blastlab.serviceblbnavi.rest.RestApplication.MAX_FILE_SIZE_LIMIT_IN_MEGABYTES;
-
 public enum FileMessagePack {
 	FILE_001("The uploaded file should have jpg or png extension."),
-	FILE_002("The uploaded file exceeds " + MAX_FILE_SIZE_LIMIT_IN_MEGABYTES + " MB.");
+	FILE_002("The uploaded file exceeds 5 MB.");
 
 	private String message;
 
