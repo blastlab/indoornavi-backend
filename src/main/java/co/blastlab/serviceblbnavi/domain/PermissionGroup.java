05fda2f0a16bfc68283021868437c8677a393b2a
Karol Gebert <kgebert@blastlab.co>
2017-07-12 14:06:26 +0200

    CRUD for permission groups, modified create for User due to frontend modifications navi-135

--- src/main/java/co/blastlab/serviceblbnavi/domain/PermissionGroup.java
+++ src/main/java/co/blastlab/serviceblbnavi/domain/PermissionGroup.java
@@ -3,6 +3,7 @@ package co.blastlab.serviceblbnavi.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import java.util.ArrayList;
@@ -14,6 +15,6 @@ import java.util.List;
 public class PermissionGroup extends TrackedEntity {
 	private String name;
 
-	@ManyToMany
+	@ManyToMany(cascade = { CascadeType.MERGE })
 	private List<Permission> permissions = new ArrayList<>();
 }
