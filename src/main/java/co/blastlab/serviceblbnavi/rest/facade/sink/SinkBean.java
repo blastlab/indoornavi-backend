38cf3f96bacf4a2cd43cce116d0bc06c235185d8
Karol Gebert <kgebert@blastlab.co>
2017-09-05 09:08:02 +0200

    Added verified, floor, anchors, configured fields to create and update sink endpoint navi-148

--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/sink/SinkBean.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/sink/SinkBean.java
@@ -1,6 +1,9 @@
 package co.blastlab.serviceblbnavi.rest.facade.sink;
 
+import co.blastlab.serviceblbnavi.dao.repository.AnchorRepository;
+import co.blastlab.serviceblbnavi.dao.repository.FloorRepository;
 import co.blastlab.serviceblbnavi.dao.repository.SinkRepository;
+import co.blastlab.serviceblbnavi.domain.Anchor;
 import co.blastlab.serviceblbnavi.domain.Sink;
 import co.blastlab.serviceblbnavi.dto.sink.SinkDto;
 import org.apache.http.HttpStatus;
@@ -9,6 +12,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +21,12 @@ public class SinkBean implements SinkFacade {
 
 	@Inject
 	private SinkRepository sinkRepository;
+
+	@Inject
+	private FloorRepository floorRepository;
+
+	@Inject
+	private AnchorRepository anchorRepository;
 
 	@Override
 	public List<SinkDto> findAll() {
@@ -46,6 +56,14 @@ public class SinkBean implements SinkFacade {
 		sinkEntity.setName(sink.getName());
 		sinkEntity.setShortId(sink.getShortId());
 		sinkEntity.setLongId(sink.getLongId());
+		sinkEntity.setVerified(sink.getVerified());
+		sinkEntity.setFloor(sink.getFloorId() != null ? floorRepository.findOptionalById(sink.getFloorId()).orElseThrow(EntityNotFoundException::new) : null);
+		List<Anchor> anchors = new ArrayList<>();
+		sink.getAnchors().forEach(anchorDto -> {
+			anchors.add(anchorRepository.findById(anchorDto.getId()).orElseThrow(EntityNotFoundException::new));
+		});
+		sinkEntity.setAnchors(anchors);
+		sinkEntity.setConfigured(sink.getConfigured());
 		sinkRepository.save(sinkEntity);
 		return new SinkDto(sinkEntity);
 	}
