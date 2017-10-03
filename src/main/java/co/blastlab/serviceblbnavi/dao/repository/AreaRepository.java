78057e8b3b1e29b5262b363756aadc4802bf5f54
Karol Gebert <kgebert@blastlab.co>
2017-10-03 12:45:10 +0200

    Cleaning names of system objects, new endpoint to get all areas for specific floor, added filter to area event controller to send only events per specific floor navi-152

--- src/main/java/co/blastlab/serviceblbnavi/dao/repository/AreaRepository.java
+++ src/main/java/co/blastlab/serviceblbnavi/dao/repository/AreaRepository.java
@@ -1,12 +1,15 @@
 package co.blastlab.serviceblbnavi.dao.repository;
 
 import co.blastlab.serviceblbnavi.domain.Area;
+import co.blastlab.serviceblbnavi.domain.Floor;
 import org.apache.deltaspike.data.api.EntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AreaRepository extends EntityRepository<Area, Long> {
 	Optional<Area> findOptionalById(Long id);
+	List<Area> findByFloor(Floor floor);
 }
