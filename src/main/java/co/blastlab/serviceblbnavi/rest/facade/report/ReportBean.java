585830f5009db81445c4b1fbf0a93befe1e082e7
Karol Gebert <kgebert@blastlab.co>
2017-12-08 14:36:29 +0100

    Endpoint to get occurred area events navi-167

--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/report/ReportBean.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/report/ReportBean.java
@@ -1,21 +1,27 @@
 package co.blastlab.serviceblbnavi.rest.facade.report;
 
+import co.blastlab.serviceblbnavi.dao.repository.AreaRepository;
 import co.blastlab.serviceblbnavi.dao.repository.CoordinatesRepository;
+import co.blastlab.serviceblbnavi.domain.Area;
+import co.blastlab.serviceblbnavi.domain.AreaConfiguration;
 import co.blastlab.serviceblbnavi.dto.report.CoordinatesDto;
 import co.blastlab.serviceblbnavi.dto.report.ReportFilterDto;
+import co.blastlab.serviceblbnavi.socket.area.AreaEvent;
 import com.google.common.collect.Range;
 
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReportBean implements ReportFacade {
 
 	@Inject
 	private CoordinatesRepository coordinatesRepository;
+
+	@Inject
+	private AreaRepository areaRepository;
 
 	@Override
 	public List<CoordinatesDto> getCoordinates(ReportFilterDto filter) {
@@ -25,4 +31,35 @@ public class ReportBean implements ReportFacade {
 		return coordinatesRepository.findByFloorIdAndInRange(filter.getFloorId(), range)
 			.stream().map(CoordinatesDto::new).collect(Collectors.toList());
 	}
+
+	@Override
+	public List<AreaEvent> getAreaEvents(ReportFilterDto filter) {
+		Map<Integer, List<Area>> tagInArea = new HashMap<>();
+		List<AreaEvent> events = new ArrayList<>();
+		List<CoordinatesDto> coordinates = getCoordinates(filter);
+		coordinates.forEach(coordinatesDto -> {
+			List<Area> areas = areaRepository.findAreasThePointIsWithin(coordinatesDto.getPoint().getX(), coordinatesDto.getPoint().getY());
+			Integer tagShortId = coordinatesDto.getTagShortId();
+			LocalDateTime date = LocalDateTime.ofInstant(coordinatesDto.getDate().toInstant(), ZoneId.systemDefault());
+			if (tagInArea.containsKey(tagShortId) && !areas.isEmpty()) {
+				// the tag has entered the areas
+				areas.forEach(area -> {
+					events.add(new AreaEvent(AreaConfiguration.Mode.ON_ENTER, area.getId(), area.getName(), tagShortId, date));
+				});
+				tagInArea.put(tagShortId, areas);
+			} else if (tagInArea.containsKey(tagShortId) && areas.isEmpty()) {
+				// the tag has left the areas
+				tagInArea.get(tagShortId).forEach(area -> {
+					events.add(new AreaEvent(AreaConfiguration.Mode.ON_LEAVE, area.getId(), area.getName(), tagShortId, date));
+				});
+				tagInArea.get(tagShortId).clear();
+			} else if (!tagInArea.containsKey(tagShortId) && !areas.isEmpty()) {
+				// the tag has already been in the areas
+				tagInArea.put(tagShortId, areas);
+			}
+		});
+
+		return events;
+	}
+
 }
