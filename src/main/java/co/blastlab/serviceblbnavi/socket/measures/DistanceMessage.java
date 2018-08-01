cdcd2ae6d402d23ec2264697c6aa2362e2a1a596
Karol Gebert <kgebert@blastlab.co>
2018-08-01 13:53:46 +0200

    Added toString on distance message navi-316

--- src/main/java/co/blastlab/serviceblbnavi/socket/measures/DistanceMessage.java
+++ src/main/java/co/blastlab/serviceblbnavi/socket/measures/DistanceMessage.java
@@ -3,10 +3,12 @@ package co.blastlab.serviceblbnavi.socket.measures;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@ToString
 public class DistanceMessage {
 	private Integer did1;
 	private Integer did2;
