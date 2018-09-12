74abaf5b700b4407142e73197acaf8cd8eb24366
Karol Gebert <kgebert@blastlab.co>
2018-09-12 12:56:18 +0200

    Fixed phone coordinates facade

--- src/main/java/co/blastlab/serviceblbnavi/socket/measures/Point3D.java
+++ src/main/java/co/blastlab/serviceblbnavi/socket/measures/Point3D.java
@@ -2,11 +2,13 @@ package co.blastlab.serviceblbnavi.socket.measures;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Point3D {
 	private Integer x;
 	private Integer y;
