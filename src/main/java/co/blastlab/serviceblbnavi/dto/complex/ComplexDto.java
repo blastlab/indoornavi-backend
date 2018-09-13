7474a049511df363c782e2b983e689bcc96e64f9
Karol Gebert <kgebert@blastlab.co>
2018-09-13 13:28:57 +0200

    Get complex tree env-397

--- src/main/java/co/blastlab/serviceblbnavi/dto/complex/ComplexDto.java
+++ src/main/java/co/blastlab/serviceblbnavi/dto/complex/ComplexDto.java
@@ -2,6 +2,7 @@ package co.blastlab.serviceblbnavi.dto.complex;
 
 import co.blastlab.serviceblbnavi.domain.Complex;
 import co.blastlab.serviceblbnavi.dto.building.BuildingDto;
+import co.blastlab.serviceblbnavi.dto.floor.FloorDto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
@@ -31,16 +32,32 @@ public class ComplexDto {
 		this.setName(complex.getName());
 	}
 
-
 	@Getter
 	@Setter
 	@NoArgsConstructor
 	public static class WithBuildings extends ComplexDto {
+
 		private List<BuildingDto> buildings = new ArrayList<>();
 
 		public WithBuildings(Complex complex) {
 			super(complex);
 			complex.getBuildings().forEach(building -> this.getBuildings().add(new BuildingDto(building)));
+		}
+
+		@Getter
+		@Setter
+		@NoArgsConstructor
+		public static class WithFloors extends WithBuildings {
+
+			private List<FloorDto> floors = new ArrayList<>();
+
+			public WithFloors(Complex complex) {
+				super(complex);
+				complex.getBuildings().forEach(building -> building.getFloors().forEach(floor -> {
+						this.getFloors().add(new FloorDto(floor));
+					})
+				);
+			}
 		}
 	}
 }
