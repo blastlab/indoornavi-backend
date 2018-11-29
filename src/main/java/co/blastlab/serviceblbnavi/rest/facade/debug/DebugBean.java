a54259f73606b41ab2f51596649c91e0c38ee458
Karol Gebert <kgebert@blastlab.co>
2018-11-29 09:09:34 +0100

    added file name and logging, fixed seconds in timestamp navi-512

--- src/main/java/co/blastlab/serviceblbnavi/rest/facade/debug/DebugBean.java
+++ src/main/java/co/blastlab/serviceblbnavi/rest/facade/debug/DebugBean.java
@@ -6,6 +6,7 @@ import co.blastlab.serviceblbnavi.domain.DebugReport;
 import co.blastlab.serviceblbnavi.dto.report.UwbCoordinatesDto;
 import co.blastlab.serviceblbnavi.socket.measures.DistanceMessage;
 import co.blastlab.serviceblbnavi.socket.measures.MeasuresWebSocket;
+import co.blastlab.serviceblbnavi.utils.Logger;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpStatus;
@@ -19,9 +20,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Stateless
@@ -39,22 +44,27 @@ public class DebugBean implements DebugFacade {
 	@Inject
 	private SinkRepository sinkRepository;
 
+	@Inject
+	private Logger logger;
+
 	private Stream.Builder<String> coordinatesStreamBuilder;
 	private Stream.Builder<String> rawMeasuresStreamBuilder;
 
 	public void rawMeasureEndpoint(@Observes DistanceMessage distanceMessage) throws JsonProcessingException {
+		logger.trace("Adding raw measure to file: {}", distanceMessage.toString());
 		rawMeasuresStreamBuilder.add(
-			String.format("%s %s",
-				new Date().getTime() / 1000,
+			String.format("%s %s\n",
+				getCurrentTimeInSeconds(),
 				objectMapper.writeValueAsString(distanceMessage)
 			)
 		);
 	}
 
 	public void calculatedCoordinatesEndpoint(@Observes UwbCoordinatesDto coordinatesDto) {
+		logger.trace("Adding coordinates to file: {}", coordinatesDto.toString());
 		coordinatesStreamBuilder.add(
-			String.format("%s; %s; %s; %s; %s",
-				coordinatesDto.getDate().getTime() / 1000,
+			String.format("%s; %s; %s; %s; %s\n",
+				convertToSeconds(coordinatesDto.getDate().getTime()),
 				Integer.toHexString(coordinatesDto.getTagShortId()),
 				coordinatesDto.getPoint().getX(),
 				coordinatesDto.getPoint().getY(),
@@ -70,7 +80,7 @@ public class DebugBean implements DebugFacade {
 
 	@Override
 	public Response download(Long id) {
-//		logger.debug("Trying to download image id {}", id);
+		logger.trace("Trying to download debug report file: {}", id);
 		Optional<DebugReport> debugReportOptional = debugReportRepository.findOptionalById(id);
 		if (debugReportOptional.isPresent()) {
 			DebugReport debugReport = debugReportOptional.get();
@@ -83,13 +93,16 @@ public class DebugBean implements DebugFacade {
 				}
 			};
 
-			return Response.ok(stream, MediaType.APPLICATION_OCTET_STREAM).header("Content-Disposition", "attachment;filename=classes.jar").build();
+			logger.trace("Streaming debug report file: {}", debugReport.getName());
+			return Response.ok(stream, MediaType.APPLICATION_OCTET_STREAM)
+				.header("Content-Disposition", String.format("attachment;filename=%s", debugReport.getName())).build();
 		}
 		throw new EntityNotFoundException();
 	}
 
 	@Override
 	public Response start(Long sinkId) throws IOException {
+		logger.trace("Starting to collect debug files for sink {}", sinkId);
 		measuresWebSocket.setDebugMode(true);
 
 		startCoordinatesFile();
@@ -101,9 +114,12 @@ public class DebugBean implements DebugFacade {
 	@Override
 	public Response stop() throws IOException {
 		if (coordinatesStreamBuilder == null || rawMeasuresStreamBuilder == null) {
+			logger.trace("You have to run start endpoint first.");
 			return Response.status(HttpStatus.SC_BAD_REQUEST).build();
 		}
 		measuresWebSocket.setDebugMode(false);
+
+		logger.trace("Save debug report files");
 
 		saveFile(coordinatesStreamBuilder, DebugReport.ReportType.COORDINATES);
 		saveFile(rawMeasuresStreamBuilder, DebugReport.ReportType.RAW);
@@ -123,16 +139,16 @@ public class DebugBean implements DebugFacade {
 		rawMeasuresStreamBuilder = Stream.builder();
 		sinkRepository.findOptionalById(sinkId).ifPresent(sink -> {
 			rawMeasuresStreamBuilder.add(
-				String.format("%s sink %s %s",
-					new Date().getTime() / 1000,
+				String.format("%s sink %s %s\n",
+					getCurrentTimeInSeconds(),
 					Integer.toHexString(sink.getShortId()),
 					sink.getMac() == null ? 0 : sink.getMac()
 				)
 			);
 			sink.getAnchors().forEach((anchor -> {
 				rawMeasuresStreamBuilder.add(
-					String.format("%s anchor %s %s %s %s",
-						new Date().getTime() / 1000,
+					String.format("%s anchor %s %s %s %s\n",
+						getCurrentTimeInSeconds(),
 						Integer.toHexString(anchor.getShortId()),
 						anchor.getX(),
 						anchor.getY(),
@@ -145,12 +161,25 @@ public class DebugBean implements DebugFacade {
 
 	private void saveFile(Stream.Builder<String> streamBuilder, DebugReport.ReportType type) {
 		Stream<String> stream = streamBuilder.build();
-		Byte[] bytes = stream.toArray(Byte[]::new);
+		String stringData = stream.collect(Collectors.joining());
 		stream.close();
-		byte[] data = new byte[bytes.length];
-		for (int i = 0; i < bytes.length; i++) {
-			data[i] = bytes[i];
-		}
-		debugReportRepository.save(new DebugReport(data, type));
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+		debugReportRepository.save(
+			new DebugReport(
+				stringData.getBytes(),
+				String.format("%s-%s.txt", type.toString(), formatter.format(new Date())),
+				type
+			)
+		);
 	}
+
+	private String convertToSeconds(long milis) {
+		DecimalFormat df = new DecimalFormat("0.0000");
+		return df.format((double) milis / 1000);
+	}
+
+	private String getCurrentTimeInSeconds() {
+		return convertToSeconds(new Date().getTime());
+	}
+
 }
