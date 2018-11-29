aea937838acffa1bd7c46f90eefaf61a46673062
Karol Gebert <kgebert@blastlab.co>
2018-11-29 09:09:34 +0100

    added file name and logging, fixed seconds in timestamp navi-512

--- src/main/java/co/blastlab/serviceblbnavi/domain/DebugReport.java
+++ src/main/java/co/blastlab/serviceblbnavi/domain/DebugReport.java
@@ -19,6 +19,8 @@ public class DebugReport extends TrackedEntity {
 	@Basic(fetch = FetchType.LAZY)
 	private byte[] data;
 
+	private String name;
+
 	@Enumerated(EnumType.STRING)
 	private ReportType reportType;
 
