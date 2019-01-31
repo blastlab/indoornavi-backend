129b4500c58542097e23ec26f7a6e38b1b709a8d
kgebert <kgebert@blastlab.co>
2019-01-31 15:09:13 +0100

    Removed measurementDate navi-568

--- src/main/java/co/blastlab/serviceblbnavi/dto/phone/PhoneCoordinatesDto.java
+++ src/main/java/co/blastlab/serviceblbnavi/dto/phone/PhoneCoordinatesDto.java
@@ -2,9 +2,11 @@ package co.blastlab.serviceblbnavi.dto.phone;
 
 import co.blastlab.serviceblbnavi.domain.PhoneCoordinates;
 import co.blastlab.serviceblbnavi.dto.CoordinatesDto;
+import co.blastlab.serviceblbnavi.utils.DateConverter;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -13,11 +15,9 @@ import java.time.LocalDateTime;
 @ToString(callSuper = true)
 public class PhoneCoordinatesDto extends CoordinatesDto {
 	private Long phoneId;
-	private LocalDateTime measurementTime;
 
 	public PhoneCoordinatesDto(PhoneCoordinates phoneCoordinates) {
 		super(phoneCoordinates);
 		this.phoneId = phoneCoordinates.getPhone() != null ? phoneCoordinates.getPhone().getId() : null;
-		this.measurementTime = phoneCoordinates.getMeasurementTime();
 	}
 }
