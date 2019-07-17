37891f0f2ee57cf53e65ad32d43b58fa4047afa4
Paweł Sternicki <psternicki@blastlab.co>
2019-07-17 15:24:31 +0200

    start-server.sh script modified with command that compiles coordinates-calculator

--- coordinates-calculator/start-server.sh
+++ coordinates-calculator/start-server.sh
@@ -2,9 +2,17 @@
 
 : ${APP_ENVIRONMENT:=development}
 : ${OPT_PARAMETERS:=""}
+: ${DB_HOST:="localhost:3306"}
 
 if [[ ${APP_ENVIRONMENT} = "development" ]]; then
     OPT_PARAMETERS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=y"
 fi
 
-java ${OPT_PARAMETERS} -Xms512m -Xmx2048m -jar app/coordinates-calculator.jar
+if [ ! -f target/coordinates-calculator.jar ];then
+    cp pom.xml pom.xml.backup
+    sed -i "s|localhost:3306|${DB_HOST}|" pom.xml
+    mvn clean install spring-boot:repackage
+    mv pom.xml.backup pom.xml
+fi
+
+java ${OPT_PARAMETERS} -Xms512m -Xmx2048m -jar target/coordinates-calculator.jar
